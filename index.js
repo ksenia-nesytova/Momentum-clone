07b3be99bc98d302dd5833cb32e6c15e2693a748
ksenia-nesytova <alleheldentot@gmail.com>
2021-07-15 15:44:00 +0700

    photographer name is now displayed

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-fetch('https://api.unsplash.com/photos/random/?client_id=LG2rIkefC7PHx4Xj15Sp0hTXf5v3Z2jq11mlut_0H3k')
+let author = document.getElementById("author");
+
+fetch('https://api.unsplash.com/photos/random?client_id=LG2rIkefC7PHx4Xj15Sp0hTXf5v3Z2jq11mlut_0H3k&orientation=landscape&query=snow')
 .then(response => response.json())
-.then(data =>  document.body.style.backgroundImage = `url('${data.urls.regular }')`)
+.then(data => {
+  document.body.style.backgroundImage = `url('${data.urls.regular}')`
+  author.innerText = data.user.name
+})
